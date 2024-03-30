5090d967d2b60fc95b556d3749b65a9ff1d98fd0
naaa <na0908nna@gmail.com>
2024-03-30 11:51:26 +0300

    first commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button className='btn btn-primary' onClick={() => console.log("Click")}>
+                Add to Cart
+            </button>
+        </div>
+    )
+}
+
+export default AddToCart
